8c0371a598c83b5f75d63e7806955aadd9fb2746
Dean Stanley-Hunt <30317335+DeanSH@users.noreply.github.com>
2019-11-17 15:37:33 +1300

    First Commit

---
+++ TheMaze/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' insieme di attributi. Per modificare le informazioni associate a un assembly
+' occorre quindi modificare i valori di questi attributi.
+
+' Rivedere i valori degli attributi dell'assembly
+
+<Assembly: AssemblyTitle("Angry Birds 3")>
+<Assembly: AssemblyDescription("Maze Runner")>
+<Assembly: AssemblyCompany("R4.Productionz")>
+<Assembly: AssemblyProduct("Angry Birds 3")>
+<Assembly: AssemblyCopyright("Copyright © R4.Productionz 2017")>
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID che segue verrà utilizzato per creare l'ID della libreria dei tipi
+<Assembly: Guid("f97f6e07-e64c-42e1-8da5-ae217524dcc1")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Numero di versione principale
+'      Numero di versione secondario 
+'      Numero build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
+' utilizzando l'asterisco (*) come descritto di seguito:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
